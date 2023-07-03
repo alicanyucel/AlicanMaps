4ac7f14a2be584b7e9fb54c977561e02ca38de08
Ali Can Yücel (The Coderman) <32604562+alicanyucel@users.noreply.github.com>
2023-07-03 20:33:20 +0300

    Update mapquest.service.ts

--- src/app/services/mapquest.service.ts
+++ src/app/services/mapquest.service.ts
@@ -7,7 +7,7 @@ import 'rxjs/add/operator/map';
 @Injectable()
 
 export class MapQuestService {
-      API_KEY = 333333//API KEY; api key alınacak/////
+      API_KEY =:8sm0Wj5hBzry0HxOhCJ2fyilcch54hE6;
       url= 'http://open.mapquestapi.com/directions/v2/route';
   constructor(private http: HttpClient) { }
 
